5410dfbb6ce40c868dfa06e54cfd4ec71cc31eb3
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2024-12-01 00:27:14 +0530

    Time: 0 ms (100%), Space: 44.7 MB (29.33%) - LeetHub

---
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] result = new int[2*n];
+        for(int i=0;i<n;i++){
+            result[i+i]=nums[i];
+            result[i+i+1] = nums[i+n];
+        }
+        return result;
+    }
+}
