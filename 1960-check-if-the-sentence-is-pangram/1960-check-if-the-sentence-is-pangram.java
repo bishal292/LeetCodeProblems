2929763250f47f59727a0a19d746189445acfa36
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2024-12-02 02:26:44 +0530

    Time: 2 ms (74.01%), Space: 41.7 MB (24.54%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -1,9 +1,28 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        Set hs = new HashSet<>();
-        for(char ch: sentence.toCharArray()){
-            hs.add(ch);
+
+        // // Simple Solution by using Set to store the characters from the sentence and
+        // returning true if length of set is 26 else false.
+        // Set hs = new HashSet<>();
+        // for(char ch: sentence.toCharArray()){
+        // hs.add(ch);
+        // }
+        // return hs.size()==26?true:false;
+
+        // Optimized Solution using BitMasking:
+
+        int n = sentence.length();
+        int seen = 0;
+
+        for (int i = 0; i < n; i++) {
+            int index = sentence.charAt(i) - 'a';
+            int currentBit = 1 << index;
+            seen = seen | currentBit;
         }
-        return hs.size()==26?true:false;
+        // if all the letters are present then seen = 1....(26times)
+        // seen+1 = 1000(0 should be present 26 times) ---> 1<<26
+        if ((1 << 26) - 1 == seen)
+            return true;
+        return false;
     }
 }
