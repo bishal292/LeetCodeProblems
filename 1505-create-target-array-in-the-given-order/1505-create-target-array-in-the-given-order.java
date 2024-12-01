27a453ccb8245431e27cdb4d8f58949e86a43397
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2024-12-02 02:18:26 +0530

    Time: 0 ms (100%), Space: 42.1 MB (13.17%) - LeetHub

--- 1505-create-target-array-in-the-given-order/1505-create-target-array-in-the-given-order.java
+++ 1505-create-target-array-in-the-given-order/1505-create-target-array-in-the-given-order.java
@@ -1,15 +1,25 @@
 class Solution {
     public int[] createTargetArray(int[] nums, int[] index) {
-        int len = nums.length;
-        int[] result = new int[len];
-        for (int i = 0; i < len; i++) {
-            // Move elements to the right starting from the last index to avoid overwriting
-            shiftToRight(index[i],result);
-            // Insert the current number at the given index
-            result[index[i]] = nums[i];
-        }
+        // int len = nums.length;
+        // int[] result = new int[len];
+        // for (int i = 0; i < len; i++) {
+        //     // Move elements to the right starting from the last index to avoid overwriting
+        //     shiftToRight(index[i],result);
+        //     // Insert the current number at the given index
+        //     result[index[i]] = nums[i];
+        // }
 
-        return result;
+        // return result;
+
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=0; i<nums.length; i++){
+            list.add(index[i], nums[i]);
+        }
+        int target[]=new int[list.size()];
+        for(int i=0; i<list.size(); i++){
+            target[i]=list.get(i);
+        }
+        return target;
     }
     void shiftToRight(int index,int[] arr){
         for(int i=arr.length-1;i>index;i--){
