5f85017cbfc8a65c2e0ba543735ab2d1e8034c27
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2024-12-21 00:27:16 +0530

    Time: 3 ms (8.39%), Space: 57.5 MB (39.14%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,21 +1,45 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        // 1 2 3 4 5 6 7        k=3
-        // 0 1 2 3 4 5 6
-        // 3 4 5 6 0 1 2
+
         int n= nums.length;
         k = k % n;
         if( n == 1 || k == 0 )return;
-        int[] temp = new int[n];
+
+        // using temp Array to store the result;
+
+        // int[] temp = new int[n];
         
-        for(int i=0;i<k;i++){
-            temp[i]=nums[n-k+i];
-        }
-        for(int i = k,j=0;i<n;i++){
-            temp[i] = nums[j++];
-        }
-        for(int i=0;i<n;i++){
-            nums[i] = temp[i];
+        // for(int i=0;i<k;i++){
+        //     temp[i]=nums[n-k+i];
+        // }
+        // for(int i = k;i<n;i++){
+        //     temp[i] = nums[i-k];
+        // }
+        // for(int i=0;i<n;i++){
+        //     nums[i] = temp[i];
+        // }
+
+        // Optimal Solution (inspace rotation);
+        // 1 2 3 4 5 6 7
+        // 7 6 5 4 3 2 1
+        // 5 6 7 4 3 2 1
+        // 5 6 7 1 2 3 4
+
+        // Reverse the entire array;
+        reverseInRanges(nums, 0,n-1);
+
+        // Now reverse the first k elements;
+        reverseInRanges(nums, 0 , k-1);
+        
+        // Reverse the remaining elements after first k elements
+        reverseInRanges(nums, k, n-1);
+        
+    }
+    public void reverseInRanges(int[] nums , int start, int end){
+        while(start < end){
+            nums[start] += nums[end];
+            nums[end] = nums[start] - nums[end];
+            nums[start++] -= nums[end--];
         }
     }
 }
