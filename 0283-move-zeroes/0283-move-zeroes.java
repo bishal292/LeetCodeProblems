a758c8b9b9603cc1e76dfc2835386ea374188ac3
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2024-12-21 09:36:23 +0530

    Time: 1 ms (99.8%), Space: 46.2 MB (19.55%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,21 +1,37 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        // int n = nums.length;
+        // int i = 0, j = 0; // Two pointer where i is point to the next index and j is to point to the next
+        //                   // non-zero element;
+        // while (i < n) {
+        //     if (j < n) {
+        //         if (nums[j] == 0) {
+        //             j++;
+        //         } else {
+        //             nums[i] = nums[j];
+        //             i++;
+        //             j++;
+        //         }
+        //     }else{
+        //         nums[i]=0;
+        //         i++;
+        //     }
+        // }
+
+
+        // A better and optimized version of code.
+
         int n = nums.length;
-        int i = 0, j = 0; // Two pointer where i is point to the next index and j is to point to the next
-                          // non-zero element;
-        while (i < n) {
-            if (j < n) {
-                if (nums[j] == 0) {
-                    j++;
-                } else {
-                    nums[i] = nums[j];
-                    i++;
-                    j++;
-                }
-            }else{
-                nums[i]=0;
-                i++;
+        int lastNonZeroIndex = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i] != 0){
+                nums[lastNonZeroIndex++] = nums[i];
             }
         }
+        // after the above loop 'lastNonZeroIndex' will be pointing to the next index of the last nonZero index 
+        for(int i= lastNonZeroIndex;i<n;i++){
+            nums[i] = 0;
+        }
+
     }
 }
