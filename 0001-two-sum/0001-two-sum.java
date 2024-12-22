71fa367254e12290402a0ff0d339b974876836d8
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2024-12-22 16:09:43 +0530

    Time: 2 ms (98.91%), Space: 45.2 MB (10.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,10 +1,28 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=1;i<nums.length;i++){
-            for(int j=i;j<nums.length;j++){
-                if(nums[j]+nums[j-i]==target) return new int[]{j-i,j};
+        // for(int i=1;i<nums.length;i++){
+        //     for(int j=i;j<nums.length;j++){
+        //         if(nums[j]+nums[j-i]==target) return new int[]{j-i,j};
+        //     }
+        // }
+        // return new int[] {};
+
+        // Optimized way to solve the same using hashMap.
+
+        // A hashMap to store the currentNumber as key with it's index as value. 
+        Map<Integer,Integer> numToIdxMap = new HashMap<>(); 
+
+        int n = nums.length;
+        for(int i =0;i<n;i++){
+            int diff = target - nums[i];
+
+            // If the hashmap already contains the required number which can make the current number sums to the target then return the array index
+            if(numToIdxMap.containsKey(diff)){
+                return new int[] {i,numToIdxMap.get(diff)};
             }
+            // if the key is not present in the hash map then add the currentElement as key with it's index as value.
+            numToIdxMap.put(nums[i],i);
         }
-        return new int[] {};
+        return new int[]{};
     }
 }
