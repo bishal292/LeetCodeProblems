a881542ef57e23099737a98e2dcaa2b848a2bdb8
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2024-12-29 16:25:00 +0530

    Time: 33 ms (82.78%), Space: 51.1 MB (85.71%) - LeetHub

---
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/1744-number-of-ways-to-form-a-target-string-given-a-dictionary.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numWays(String[] words, String target) {
+        final int MOD = (int) 1e9 + 7;
+        int m = target.length();
+        int n = words[0].length();
+
+        int[][] freq = new int[26][n];
+        for (String word : words) {
+            for (int i = 0; i < n; i++) {
+                freq[word.charAt(i) - 'a'][i]++;
+            }
+        }
+
+        long[] dp = new long[m + 1];
+        dp[0] = 1;
+        for (int j = 0; j < n; j++) {
+            for (int i = m - 1; i >= 0; i--) {
+                dp[i + 1] = (dp[i + 1] + dp[i] * freq[target.charAt(i) - 'a'][j]) % MOD;
+            }
+        }
+        return (int) dp[m];
+    }
+}
