73850c165d0671ee3d26525834a98030e625c6cd
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-01-01 20:58:53 +0530

    Time: 48 ms (43.14%), Space: 66.8 MB (46.41%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -27,21 +27,21 @@ class Solution {
         Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
         int max = 0;
         for (int ele : set) {
-        int x = ele - 1;
-        int count = 0;
-        if (!set.contains(x)) {
-        while (set.contains(x + 1)) {
-        x = x + 1;
-        count++;
-        }
-        }
-        max = Math.max(max, count);
+            int x = ele - 1;
+            int count = 0;
+            if (!set.contains(x)) {
+                while (set.contains(x + 1)) {
+                    x = x + 1;
+                    count++;
+                }
+            }
+            max = Math.max(max, count);
         }
         return max;
 
-        // approach 3: using an extra space boolean array for the range (max <--> min)
+        // // approach 3: using an extra space boolean array for the range (max <--> min)
         // if (nums == null || nums.length == 0) {
-        //     return 0; // Handle empty array
+        // return 0; // Handle empty array
         // }
 
         // int max = Integer.MIN_VALUE;
@@ -49,18 +49,18 @@ class Solution {
 
         // // Find the minimum and maximum values in the array
         // for (int number : nums) {
-        //     if (number < min) {
-        //         min = number;
-        //     }
-        //     if (number > max) {
-        //         max = number;
-        //     }
+        // if (number < min) {
+        // min = number;
+        // }
+        // if (number > max) {
+        // max = number;
+        // }
         // }
 
         // // Create and populate the boolean array
         // boolean[] isObserved = new boolean[max - min + 1];
         // for (int num : nums) {
-        //     isObserved[num - min] = true;
+        // isObserved[num - min] = true;
         // }
 
         // // Find the longest consecutive sequence
@@ -68,12 +68,12 @@ class Solution {
         // int count = 0;
 
         // for (boolean flag : isObserved) {
-        //     if (flag) {
-        //         count++;
-        //     } else {
-        //         longest = Math.max(longest, count);
-        //         count = 0;
-        //     }
+        // if (flag) {
+        // count++;
+        // } else {
+        // longest = Math.max(longest, count);
+        // count = 0;
+        // }
         // }
 
         // // Check the final sequence
