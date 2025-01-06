01b56e230922fcf873b3b9e8719bb5b7311da925
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-01-06 16:29:10 +0530

    Time: 127 ms (25.35%), Space: 45.1 MB (14.93%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,30 +1,46 @@
 class Solution {
     public int[] minOperations(String boxes) {
-        int sum = 0;
+
+        // BruteForce Solution:->
         int n = boxes.length();
         int[] ans = new int[n];
-        int rightBall = 0;
-        int leftBall = 0;
-
         for(int i=0;i<n;i++){
-            if(boxes.charAt(i)=='1'){
-                sum += i;
-                rightBall++;
+            int sum = 0;
+            for(int j =0;j<n;j++){
+                if(boxes.charAt(j)=='1')
+                    sum += Math.abs(j-i);
             }
+            ans[i] = sum;
         }
 
-        // here prefixSum is the move required by all the balls at left of the current index ball.
-        // And sum is the move required by the balls at right to reach the current index.
-        int prefixSum = 0;
-        for(int i = 0;i<n;i++){
-            ans[i] = sum + prefixSum;
-            if(boxes.charAt(i) == '1'){
-                leftBall ++;
-                rightBall --;
-            }
-            sum -= rightBall;
-            prefixSum += leftBall;
-        }
+
+
+//  Optimal Solution:
+        // int sum = 0;
+        // int n = boxes.length();
+        // int[] ans = new int[n];
+        // int rightBall = 0;
+        // int leftBall = 0;
+
+        // for(int i=0;i<n;i++){
+        //     if(boxes.charAt(i)=='1'){
+        //         sum += i;
+        //         rightBall++;
+        //     }
+        // }
+
+        // // here prefixSum is the move required by all the balls at left of the current index ball.
+        // // And sum is the move required by the balls at right to reach the current index.
+        // int prefixSum = 0;
+        // for(int i = 0;i<n;i++){
+        //     ans[i] = sum + prefixSum;
+        //     if(boxes.charAt(i) == '1'){
+        //         leftBall ++;
+        //         rightBall --;
+        //     }
+        //     sum -= rightBall;
+        //     prefixSum += leftBall;
+        // }
         return ans;
     }
 }
