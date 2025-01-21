f0a8648859fb0847d8effa55a039ee8c83889e89
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-01-21 16:47:40 +0530

    Time: 3 ms (100%), Space: 57.6 MB (68.91%) - LeetHub

--- 2145-grid-game/2145-grid-game.java
+++ 2145-grid-game/2145-grid-game.java
@@ -1,22 +1,36 @@
 class Solution {
     public long gridGame(int[][] grid) {
-        Long topSum = 0l;
-        Long ans = Long.MAX_VALUE;
+        // Long topSum = 0l;
+        // Long ans = Long.MAX_VALUE;
+        // int n = grid[0].length;
+
+        // for (int num : grid[0]) {
+        //     topSum += num;
+        // }
+
+        // Long botSum = 0l;
+        // for (int i = 0; i < n; i++) {
+        //     topSum -= grid[0][i];
+
+        //     ans = Math.min(ans, Math.max(topSum, botSum));
+
+        //     botSum += grid[1][i];
+        // }
+
+        // return ans;
+
+        long ans = Long.MAX_VALUE;
+        long s1 = 0; // suffix sum of the points in the first row
+        long s2 = 0; // prefix sum of the points in the second row
+        for (int v : grid[0]) {
+            s1 += v;
+        }
         int n = grid[0].length;
-
-        for(int num : grid[0]){
-            topSum += num;
+        for (int j = 0; j < n; ++j) { // j = find optimal position to go down
+            s1 -= grid[0][j];
+            ans = Math.min(ans, Math.max(s1, s2));
+            s2 += grid[1][j];
         }
-
-        Long botSum = 0l;
-        for(int i = 0;i<n;i++){
-            topSum -= grid[0][i];
-
-            ans = Math.min(ans,Math.max(topSum,botSum));
-
-            botSum += grid[1][i];
-        }
-
         return ans;
     }
 }
