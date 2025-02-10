03c4be33fcc5da7198cb50a9f6bcdf1edc3eb701
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-02-10 22:40:56 +0530

    Time: 27 ms (100%), Space: 57.3 MB (70.59%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
@@ -1,5 +1,19 @@
 class Solution {
     public long countBadPairs(int[] nums) {
+        // HashMap<Integer,Integer> bag = new HashMap<>();
+        // int n = nums.length;
+        // long totCount = 1l * n*(n-1) / 2;
+        // long goodPair = 0L;
+
+        // for(int i = 0;i<n;i++){
+        //     int key = nums[i] - i;
+        //     int prevCount = bag.getOrDefault(key,0);
+        //     goodPair += prevCount;
+        //     bag.put(key,prevCount + 1);
+        // }
+
+        // return totCount - goodPair;
+
         Map<Integer, Integer> bag = new HashMap<>();
         long count = 0;
         for (int i = 0; i < nums.length; i++) {
