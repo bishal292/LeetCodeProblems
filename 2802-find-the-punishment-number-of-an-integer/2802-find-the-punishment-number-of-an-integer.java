367eacc5335d463c2f11dde9e14becdb8d2b7dba
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-02-16 19:41:14 +0530

    Time: 5 ms (98.44%), Space: 40.4 MB (93.46%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.java
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.java
@@ -1,11 +1,30 @@
 class Solution {
     public int punishmentNumber(int n) {
-        int[] arr = {1,9,10,36,45,55,82,91,99,100,235,297,369,370,379,414,657,675,703,756,792,909,918,945,964,990,991,999,1000};
-        int sum = 0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]<=n) sum += arr[i]*arr[i];
-            else if(arr[i]>n) break;
+        // int[] arr =
+        // {1,9,10,36,45,55,82,91,99,100,235,297,369,370,379,414,657,675,703,756,792,909,918,945,964,990,991,999,1000};
+        // int sum = 0;
+        // for(int i=0;i<arr.length;i++){
+        // if(arr[i]<=n) sum += arr[i]*arr[i];
+        // else if(arr[i]>n) break;
+        // }
+        // return sum;
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            int sqr = i * i;
+            if (canPartition(sqr, i)) {
+                ans += sqr;
+            }
         }
-        return sum;
+        return ans;
+    }
+
+    boolean canPartition(int square, int target) {
+        if (square == target)
+            return true;
+        if (square < target || target < 0)
+            return false;
+
+        return (canPartition(square / 10, target - (square % 10)) || canPartition(square / 100, target - (square % 100))
+                || canPartition(square / 1000, target - (square % 1000)));
     }
 }
