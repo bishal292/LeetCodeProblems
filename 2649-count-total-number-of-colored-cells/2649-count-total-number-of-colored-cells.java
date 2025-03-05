51be2226e7d509b6813427a605bf78673020dd38
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-03-05 14:46:35 +0530

    Time: 0 ms (100%), Space: 40.5 MB (46.72%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
@@ -1,13 +1,17 @@
 class Solution {
     public long coloredCells(int n) {
-        long ans = 1l;
-        long diff = 4l;
-        while(n > 1){
-            ans += diff;
-            diff += 4;
-            n--;
-        }
+        // long ans = 1l;
+        // long diff = 4l;
+        // while(n > 1){
+        //     ans += diff;
+        //     diff += 4;
+        //     n--;
+        // }
 
-        return ans;
+        // return ans;
+
+        // Mathematical formula solution: O(1)
+
+        return 1 + (long) 2 * n *(n-1);
     }
 }
