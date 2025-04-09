9ddc91e217f7c9b10f09559646568f576fef7ec9
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-04-09 16:24:34 +0530

    Time: 1 ms (100%), Space: 44.2 MB (89.49%) - LeetHub

--- 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.java
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.java
@@ -1,18 +1,26 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int num:nums){
-            if(num < k)return -1;
-            if(num > k)set.add(num);
+        int ans = 0;
+        boolean[] seen = new boolean[101];
+        for (int num : nums) {
+            if (num < k)
+                return -1;
+            if (num > k && !seen[num]) {
+                seen[num] = true;
+                ans++;
+            }
         }
-        // int lar = -1;
-        // int secLar= -1;
-        // for(int i = 100;i>0;i++){
-        //     if(arr[num]==0)continue;
-        //     if(lar != -1 && secLar != -1)break;
-        //     secLar = lar;
-
-        // }
-        return set.size();
+        return ans;
     }
+
+    // public int minOperations(int[] nums, int k) {
+    //     HashSet<Integer> set = new HashSet<>();
+    //     for (int num : nums) {
+    //         if (num < k)
+    //             return -1;
+    //         if (num > k)
+    //             set.add(num);
+    //     }
+    //     return set.size();
+    // }
 }
