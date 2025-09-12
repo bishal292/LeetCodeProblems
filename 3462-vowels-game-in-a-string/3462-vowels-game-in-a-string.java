acee461f781f7e5b177de2a9f1e7d4ca5c7fbbab
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-09-12 23:09:07 +0530

    Time: 11 ms (34.12%), Space: 45.4 MB (92.65%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -1,14 +1,19 @@
 class Solution {
     public boolean doesAliceWin(String s) {
-        int count = 0;
-        for(char ch:s.toCharArray()){
-            if("aeiou".indexOf(ch) != -1){
-                count++;
-            }
+        // int count = 0;
+        // for(char ch:s.toCharArray()){
+        //     if("aeiou".indexOf(ch) != -1){
+        //         count++;
+        //     }
+        // }
+        // if(count == 0){
+        //     return false;
+        // }
+        // return true;
+
+        for(char ch: s.toCharArray()){
+            if("aeiou".indexOf(ch) != -1)return true;
         }
-        if(count == 0){
-            return false;
-        }
-        return true;
+        return false;
     }
 }
