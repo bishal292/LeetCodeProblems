b6d53eebc7b74dc935bd1e7fe3f8dde349a0037d
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-10-09 20:51:51 +0530

    Time: 1 ms (32.55%), Space: 45.7 MB (22.29%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,12 +1,38 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int reqSum = 0;
-        int sum = 0;
-        for(int i =0;i<nums.length;i++){
-            reqSum += i+1;
-            sum += nums[i];
+        // Using Total Sum Approach.
+
+        // int reqSum = 0;
+        // int sum = 0;
+        // for(int i =0;i<nums.length;i++){
+        //     reqSum += i+1;
+        //     sum += nums[i];
+        // }
+        // // System.gc();
+        // return reqSum - sum;
+
+        //  Using Cyclic Sort Approach:
+        int i = 0;
+        // Sort the array in-place using cyclic sort.
+        while(i < nums.length){
+            int crtIdx = nums[i];
+            if(nums[i] == nums.length){
+                i++;
+                continue;
+            }
+            if(nums[i] != nums[crtIdx]){
+                int temp = nums[i];
+                nums[i] = nums[crtIdx];
+                nums[crtIdx] = temp;
+            }else{
+                i++;
+            }
         }
-        System.gc();
-        return reqSum - sum;
+        // Check if all the element are at correct index if not then return then index as that element is missing.
+        for(i = 0;i<nums.length;i++){
+            if(nums[i] != i)return i;
+        }
+        // and if all the element in the range is at correct index the return the length(n) itself as this is the missing one.
+        return nums.length;
     }
 }
