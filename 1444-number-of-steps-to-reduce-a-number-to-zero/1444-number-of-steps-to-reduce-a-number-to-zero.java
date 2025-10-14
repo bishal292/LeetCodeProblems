81537709697d47a83afbbc2e6b3010d11415cac4
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-10-14 19:01:27 +0530

    Time: 0 ms (100%), Space: 40.9 MB (11.81%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,14 +1,26 @@
 class Solution {
     public int numberOfSteps(int num) {
-        int steps = 0;
-        while(num != 0){
-            if((num & 1) == 0){
-                num /= 2;
-            }else{
-                num --;
-            }
-            steps++;
+        // int steps = 0;
+
+        // while(num != 0){
+        //     if((num & 1) == 0){
+        //         num /= 2;
+        //     }else{
+        //         num --;
+        //     }
+        //     steps++;
+        // }
+        // return steps;
+
+
+        // RECURSIVE APPROACH
+        return helper(num,0);
+    }
+    public int helper(int n,int steps){
+        if(n == 0)return steps;
+        if(n % 2 == 0){
+            return helper(n/2,steps + 1);
         }
-        return steps;
+        return helper(n-1,steps+1);
     }
 }
