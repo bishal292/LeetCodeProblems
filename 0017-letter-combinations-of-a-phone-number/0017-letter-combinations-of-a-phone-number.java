ef7245988f7ccc474699b2d856d90bbb0fbc0a63
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-10-21 01:31:08 +0530

    Time: 0 ms (100%), Space: 41.9 MB (89.4%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,19 +1,22 @@
 class Solution {
-    String [] alpha = {"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    final String [] alpha = {"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     public List<String> letterCombinations(String digits) {
         List<String> ans = new ArrayList<>();
-        helper("",digits,ans);
+        if(digits == null || digits.isEmpty())return ans;
+        helper(new StringBuilder(),digits,ans);
         return ans;
     }
 
-    void helper(String p,String up,List<String> list){
+    void helper(StringBuilder p,String up,List<String> list){
         if(up.isEmpty()){
-            list.add(p);
+            list.add(p.toString());
+            p = new StringBuilder();
             return;
         }
         int idx = (int)up.charAt(0) - '2';
         for(char ch : alpha[idx].toCharArray()){
-            helper(p+ch,up.substring(1),list);
+            helper(p.append(ch),up.substring(1),list);
+            p.deleteCharAt(p.length() - 1);
         }
     }
 }
