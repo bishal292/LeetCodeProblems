a48f8dff32177ba24dc02e4c95e8cc70126f6820
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-10-21 19:55:13 +0530

    Time: 0 ms (100%), Space: 41.5 MB (5.21%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,8 +1,24 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n<= 0)return false;
-        if(n==1)return true;
-        if(n % 2 == 1)return false;
-        return isPowerOfTwo(n/2);
+        // Iterative Approach
+        // if(n<= 0)return false;
+        // while(n>1){
+        //     if(n%2 == 1)return false;
+        //     n /= 2;
+        // }
+        // return true;
+        
+        
+        // Recursive Approach
+        
+        // if(n<= 0)return false;
+        // if(n==1)return true;
+        // if(n % 2 == 1)return false;
+        // return isPowerOfTwo(n/2);
+
+
+        // Most Optimized and bit Manupulation
+
+        return (n > 0 && (n & (n-1)) == 0);
     }
 }
