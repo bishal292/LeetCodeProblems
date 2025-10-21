3582aabc7c6269049c4d070202a5606764ee8a2b
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-10-21 20:12:41 +0530

    Time: 9 ms (8.39%), Space: 44.5 MB (19.05%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,12 +1,16 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-       if(n<1){
-        return false;
-       }
-       while(n%3==0){
-        n=n/3;
-       }
-       return n==1;
-        
+        // Recursive Approach
+
+
+        // if(n <= 0)return false;
+        // if(n == 1)return true;
+        // if(n % 3 != 0)return false;
+        // return isPowerOfThree(n / 3); 
+
+        // Mathematics
+        if(n <= 0)return false;
+        double x = Math.log10(n) / Math.log10(3);
+        return x == Math.floor(x);
     }
 }
