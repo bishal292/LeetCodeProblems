37140353167df13fd5795865c66c9918ab3534e1
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-10-27 00:12:29 +0530

    Time: 0 ms (100%), Space: 45.1 MB (7.58%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.java
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -1,10 +1,26 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int[] result = new int[2*n];
-        for(int i=0;i<n;i++){
-            result[i+i]=nums[i];
-            result[i+i+1] = nums[i+n];
+        int[] ans = new int[n*2];
+
+// Simple to keep first n element as they will be on even index.
+        // for(int i = 0;i<n;i++){
+        //     ans[i+i] = nums[i]; // x1,x2,x3 and even index.
+        // }
+
+// Now iterating from last to keep the yn at itscorrect place.
+        // for(int i = nums.length -1,diff = 0;i>=n;i--,diff++){
+        //     ans[i-diff] = nums[i];
+        // }
+//   analyzing and finding a pattern where we can iterate from 0 to n keep y at its correct place.
+//   i+i+1 is the index whose value is to be changed in ith iteration and the value would be i+n
+// So placing the ans in a single loop
+
+        for(int i = 0;i<n;i++){
+            ans[i+i] = nums[i];
+            ans[i+i+1] = nums[i+n];
         }
-        return result;
+
+        
+        return ans;
     }
 }
