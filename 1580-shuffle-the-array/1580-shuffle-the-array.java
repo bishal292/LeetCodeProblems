5240404cd233e3be9964039d08f29115920983d5
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-10-27 01:16:38 +0530

    Time: 1 ms (11.8%), Space: 44.9 MB (27.35%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.java
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -15,12 +15,43 @@ class Solution {
 //   i+i+1 is the index whose value is to be changed in ith iteration and the value would be i+n
 // So placing the ans in a single loop
 
-        for(int i = 0;i<n;i++){
-            ans[i+i] = nums[i];
-            ans[i+i+1] = nums[i+n];
+        // for(int i = 0;i<n;i++){
+        //     ans[i+i] = nums[i];
+        //     ans[i+i+1] = nums[i+n];
+        // }
+        // return ans;
+
+// BY Storing the values upto n at each index from 0,n
+
+        // int factor=0;
+        // for(int num : nums){
+        //     factor = Math.max(num,factor);
+        // }
+        // factor++;
+
+        // for(int i = 0;i<n;i++){
+        //     nums[i] += nums[i+n] * factor;
+        // }
+        // for(int i= n-1;i>= 0;i--){
+        //     int x = nums[i] % factor;
+        //     int y = nums[i] / factor;
+        //     nums[2*i] = x;
+        //     nums[2*i+1] = y;
+        // }
+
+//  Using Bitwise as nums[i]'s max value can be 10^3 = 1000 which in binary can maximum holds upto 10 bits so,
+
+        for(int i= 0;i<n;i++){
+            nums[i] = nums[i] << 10; // shifts all the bits to the left 10 times making extra space so that other 10 bit can be added up.
+            nums[i] = nums[i] | nums[i+n];// will make 0th-> x1y1,1st->x2,y2
         }
 
-        
-        return ans;
+        for(int i = n-1;i>= 0;i--){
+            int y = nums[i] & (int)(Math.pow(2,10) - 1);
+            int x = nums[i] >> 10;
+            nums[2*i] = x;
+            nums[2*i+1] = y;
+        }
+        return nums;
     }
 }
