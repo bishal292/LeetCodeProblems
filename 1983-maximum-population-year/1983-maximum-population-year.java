e1e128c6e9341d55272622a4510304d7f4523f69
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-10-28 22:07:56 +0530

    Time: 0 ms (100%), Space: 41.8 MB (61.8%) - LeetHub

--- 1983-maximum-population-year/1983-maximum-population-year.java
+++ 1983-maximum-population-year/1983-maximum-population-year.java
@@ -1,39 +1,24 @@
 class Solution {
     public int maximumPopulation(int[][] logs) {
-
-        int[] pop = new int[101];
-
-        for(int[] log:logs){
-            int birth = log[0];
-            int death = log[1];
-            
-            // while(birth<death){
-            //     pop[birth-1950] += 1;
-            //     birth++;
-            // }
-
-            // Optimized Version compared to above while -> loop;
-
-            pop[birth-1950] += 1;
-            pop[death-1950] -=1;
+        // Arrays.sort(logs,(a,b) -> Integer.compare(a[0],b[0]));
+        int[] sum = new int[101];
+        int year = 1950;
+        
+        for(int[] arr:logs){
+            int bir = arr[0]-1950;
+            int dea = arr[1]-1950;
+            sum[bir]++;
+            sum[dea]--;
         }
 
-        int curYear = 0, maxPop=0;
-        int curPop =0;
-        for(int i=0;i<101;i++){
-            // if(maxPop < pop[i] && curYear < i+1950 ){
-            //     curYear = i+1950;
-            //     maxPop = pop[i];
-            // }
-
-            //  Conditions Check according to the above change 
-            curPop += pop[i];
-            if(maxPop < curPop && curYear < i + 1950){
-                maxPop = curPop;
-                curYear = i + 1950;
+        int maxSum = sum[0];
+        for(int i = 1;i<101;i++){
+            sum[i] += sum[i-1];
+            if(sum[i] > maxSum){
+                maxSum = sum[i];
+                year = i+1950;
             }
         }
-
-        return curYear;
+        return year;
     }
 }
