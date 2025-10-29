6f9d9723f98dd7414986b746de4116bacebdfa04
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-10-29 13:42:08 +0530

    Time: 0 ms (100%), Space: 41.3 MB (9.69%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.java
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.java
@@ -5,6 +5,7 @@ class Solution {
         // }
         // return n;
 
+
         // int mask = 1;
         // while(mask <= n){
         //     mask <<= 1;
@@ -12,9 +13,24 @@ class Solution {
         // return mask -1;
 
 
-        if ((n & (n + 1)) == 0) return n; // if n is already all 1's
-        int highest = Integer.highestOneBit(n); // like 1000
-        int mask = (highest << 1) - 1;          // make it 1111
-        return mask;
+        // if ((n & (n + 1)) == 0) return n; // if n is already all 1's
+        // int highest = Integer.highestOneBit(n); // like 1000
+        // int mask = (highest << 1) - 1;          // make it 1111
+        // return mask;
+
+
+        // if ((n & (n + 1)) == 0) return n;
+        // int bits = 32 - Integer.numberOfLeadingZeros(n); // get all the starting bits which are 0
+        // return (1 << bits) - 1;
+
+
+
+        int x = n;
+        x |= (x >> 1);
+        x |= (x >> 2);
+        x |= (x >> 4);
+        x |= (x >> 8);
+        x |= (x >> 16);
+        return x;
     }
 }
