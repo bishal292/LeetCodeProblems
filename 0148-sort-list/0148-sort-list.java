06ae79b6b1844e128464cc9a07dbf44ee78f5cfd
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-11-06 23:28:47 +0530

    Time: 1 ms (100%), Space: 63.4 MB (5.52%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -8,6 +8,8 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ /**
 class Solution {
     public ListNode sortList(ListNode head){
         if(head == null || head.next == null){
@@ -55,5 +57,46 @@ class Solution {
         }
         if (head2 != null) head.next = head2;
         return ans.next;
+    }
+}
+*/
+
+
+
+
+class Solution {
+    static {
+        for (int i = 0; i < 500; i++) sortList(null);
+    }
+
+    public static ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        ListNode temp = head;
+        while (temp != null) {
+            min = Math.min(min, temp.val);
+            max = Math.max(max, temp.val);
+            temp = temp.next;
+        }
+
+
+        int[] freq = new int[max - min + 1];
+        temp = head;
+        while (temp != null) {
+            freq[temp.val - min]++;
+            temp = temp.next;
+        }
+
+        temp = head;
+        for (int i = 0; i < freq.length; i++) {
+            while (freq[i]-- > 0) {
+                temp.val = i + min;
+                temp = temp.next;
+            }
+        }
+
+        return head;
     }
 }
