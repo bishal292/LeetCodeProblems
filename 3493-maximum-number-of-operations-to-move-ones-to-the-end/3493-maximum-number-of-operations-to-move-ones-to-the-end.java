e5ea20c56e73c5eb55aebc23146c0d08ef57b777
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-11-13 21:08:15 +0530

    Time: 9 ms (18.52%), Space: 47.3 MB (25.93%) - LeetHub

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.java
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.java
@@ -1,23 +1,36 @@
 class Solution {
     public int maxOperations(String s) {
-        int ans = 0;
-        int i = 0;
-        int count = 0;
+        // int ans = 0;
+        // int i = 0;
+        // int count = 0;
+        // int n = s.length();
+        // while (i < n) {
+        //     if (s.charAt(i) == '1') {
+        //         while (i < n && s.charAt(i) == '1') {
+        //             count++;
+        //             i++;
+        //         }
+        //         if (i < n && s.charAt(i) == '0'){// next zero encountered.
+        //             while (i < n && s.charAt(i) == '0') {
+        //                 i++;
+        //             }
+        //             ans += count;
+        //         }
+        //     } else
+        //         i++;
+        // }
+
+        // return ans;
+
+
+        int ans = 0, ones = 0;
         int n = s.length();
-        while (i < n) {
-            if (s.charAt(i) == '1') {
-                while (i < n && s.charAt(i) == '1') {
-                    count++;
-                    i++;
-                }
-                if (i < n && s.charAt(i) == '0'){// next zero encountered.
-                    while (i < n && s.charAt(i) == '0') {
-                        i++;
-                    }
-                    ans += count;
-                }
-            } else
-                i++;
+
+        for (int i = 0; i < n - 1; i++) {
+            if(s.charAt(i) == '1')ones++;
+            if (s.charAt(i) == '1' && s.charAt(i + 1) == '0') {
+                ans += ones;
+            }
         }
 
         return ans;
