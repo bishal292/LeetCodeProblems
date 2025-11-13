b94b649107b4e027b01edf027284e558e408d160
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-11-13 21:14:20 +0530

    Time: 7 ms (78.52%), Space: 47.1 MB (27.41%) - LeetHub

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.java
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.java
@@ -1,38 +1,34 @@
 class Solution {
     public int maxOperations(String s) {
-        // int ans = 0;
-        // int i = 0;
-        // int count = 0;
-        // int n = s.length();
-        // while (i < n) {
-        //     if (s.charAt(i) == '1') {
-        //         while (i < n && s.charAt(i) == '1') {
-        //             count++;
-        //             i++;
-        //         }
-        //         if (i < n && s.charAt(i) == '0'){// next zero encountered.
-        //             while (i < n && s.charAt(i) == '0') {
-        //                 i++;
-        //             }
-        //             ans += count;
-        //         }
-        //     } else
-        //         i++;
-        // }
-
-        // return ans;
-
-
-        int ans = 0, ones = 0;
+        int ans = 0;
+        int i = 0;
+        int count = 0;
         int n = s.length();
-
-        for (int i = 0; i < n - 1; i++) {
-            if(s.charAt(i) == '1')ones++;
-            if (s.charAt(i) == '1' && s.charAt(i + 1) == '0') {
-                ans += ones;
+        while (i < n) {
+            while (i < n && s.charAt(i) == '1') {
+                count++;
+                i++;
+            }
+            if (i < n && s.charAt(i) == '0') {// next zero encountered.
+                ans += count;
+            }
+            while (i < n && s.charAt(i) == '0') {
+                i++;
             }
         }
 
         return ans;
+
+        // int ans = 0, ones = 0;
+        // int n = s.length();
+
+        // for (int i = 0; i < n - 1; i++) {
+        //     if(s.charAt(i) == '1')ones++;
+        //     if (s.charAt(i) == '1' && s.charAt(i + 1) == '0') {
+        //         ans += ones;
+        //     }
+        // }
+
+        // return ans;
     }
 }
