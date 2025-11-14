2b88c5190963b0b7ae4b2b59d37c489fac71afc5
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-11-14 16:11:24 +0530

    Time: 2 ms (97.69%), Space: 43 MB (20.12%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,40 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length() % 2 == 1)return false;// odd length means 1 bracket is extra means not closed.
-        Stack<Character> stack = new Stack<>();
         int n = s.length();
-        for(int i = 0;i<n;i++){
+        if(n % 2 == 1)return false;// odd length means 1 bracket is extra means not closed.
+        // Stack<Character> stack = new Stack<>();
+        char[] stack = new char[n];
+        int top = 0;
+        
+        // for(int i = 0;i<n;i++){
+        //     char c = s.charAt(i);
+        //     if(i<n-1 && checkBrac(c,s.charAt(i+1))){
+        //         i++; // if bracket is opened and closed at the same time.
+        //     }else{
+        //         if(!stack.isEmpty() && checkBrac(stack.peek(),c)){
+        //             stack.pop();
+        //         }else{
+        //             stack.push(c);
+        //         }
+        //     }
+        // }
+
+        for(int i = 0; i< n;i++){
             char c = s.charAt(i);
-            if(i<n-1 && checkBrac(c,s.charAt(i+1))){
-                i++; // if bracket is opened and closed at the same time.
+            if(c == '(' || c == '{' || c == '['){
+                stack[top++] = c;
             }else{
-                if(!stack.isEmpty() && checkBrac(stack.peek(),c)){
-                    stack.pop();
-                }else{
-                    stack.push(c);
+                if(top <= 0)return false;
+                char p = stack[top -1];
+                if(!checkBrac(p,c)){
+                    return false;
                 }
+                top--;
             }
         }
 
-        return stack.isEmpty();
+        // return stack.isEmpty();
+        return top == 0;
     }
 
     public boolean checkBrac(char c,char n){
