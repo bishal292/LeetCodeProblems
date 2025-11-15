e8fc6558f39235150e3707be2f7b6f8e57659332
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-11-16 02:31:03 +0530

    Time: 11 ms (69.32%), Space: 47.5 MB (11.86%) - LeetHub

--- 1648-minimum-insertions-to-balance-a-parentheses-string/1648-minimum-insertions-to-balance-a-parentheses-string.java
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/1648-minimum-insertions-to-balance-a-parentheses-string.java
@@ -1,26 +1,53 @@
 class Solution {
     public int minInsertions(String s) {
+//         int ans = 0;
+//         Stack<Character> stack = new Stack<>();
+//         for (int i = 0; i < s.length(); i++) {
+//             char c = s.charAt(i);
+//             if (c == '(') { // simply add the opening brackets.
+//                 stack.push(c);
+//             } else {
+//                 if(i + 1 < s.length() && s.charAt(i+1) == ')'){ // if 2 consicutive '))' simply skips the next char without increment.
+//                     i++;
+//                 }else{ // if there is only 1 ')' the ans must be increased by 1 for the remaining ')'
+//                     ans++;
+//                 }
+
+//  // if current char is ')' then it must be closed either for a single or double '))'  
+// //  As for single ')' the ans is already handled above the in this only the ans should be handled for '(' in the stack.
+//                 if(stack.isEmpty()){
+//                     ans++;
+//                 }else{
+//                     stack.pop();
+//                 }
+//             }
+//         }
+//         return ans + (stack.size() * 2);
+
+
+
+
+// OPTIMAL SOLUTION:
+
         int ans = 0;
-        Stack<Character> stack = new Stack<>();
-        boolean mustOpen = false;
-        for (int i = 0; i < s.length(); i++) {
+        int open = 0;
+        for(int i = 0;i<s.length();i++){
             char c = s.charAt(i);
-            if (c == '(') {
-                stack.push(c);
-            } else {
-                if(i + 1 < s.length() && s.charAt(i+1) == ')'){
+            if(c == '('){
+                open++;
+            }else{
+                if(i+1 <s.length() && s.charAt(i+1) == ')'){
                     i++;
                 }else{
                     ans++;
                 }
-
-                if(stack.isEmpty()){
-                    ans++;
+                if(open > 0){
+                    open--;
                 }else{
-                    stack.pop();
+                    ans++;
                 }
             }
         }
-        return ans + (stack.size() * 2);
+        return open == 0 ? ans : ans + (open * 2);
     }
 }
