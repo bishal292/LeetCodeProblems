b02b6ab2ad6ab0eeafa38bcbe2ff0923f5f3d6cc
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2025-12-31 11:15:49 +0530

    Time: 0 ms (100%), Space: 46.9 MB (5.91%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -24,16 +24,52 @@ class Node {
 class Solution {
     public Node connect(Node root) {
         if(root == null)return null;
-        Queue<Node> que = new LinkedList<>();
-        que.offer(root);
-        while(!que.isEmpty()){
-            int n = que.size();
-            for(int i = 0;i<n;i++){
-                Node node = que.poll();
-                if(node.left != null)que.offer(node.left);
-                if(node.right != null)que.offer(node.right);
-                if(i != n-1)node.next = que.peek();
+
+        // Iterative and first Thought Approach.
+
+        // Queue<Node> que = new LinkedList<>();
+        // que.offer(root);
+        // while(!que.isEmpty()){
+        //     int n = que.size();
+        //     for(int i = 0;i<n;i++){
+        //         Node node = que.poll();
+        //         if(node.left != null)que.offer(node.left);
+        //         if(node.right != null)que.offer(node.right);
+        //         if(i != n-1)node.next = que.peek();
+        //     }
+        // }
+        // return root;
+
+        // Recursive and a better Approach.
+
+        // Node left = root.left;
+        // Node right = root.right;
+        // Node next = root.next;
+
+        // if(left != null){ // if there exists a left child then it is guranted that there is a right child also as it is a perfect binary tree.
+        //     left.next = right;
+        //     if(next != null){
+        //         right.next = next.left;
+        //     }
+        //     connect(left);
+        //     connect(right);
+        // }
+        // return root;
+
+
+        // Iterative and most Optimal 
+
+        Node leftmost = root;
+        while(leftmost.left != null){
+            Node current = leftmost;
+            while(current != null){
+                current.left.next = current.right;
+                if(current.next != null){
+                    current.right.next = current.next.left;
+                }
+                current = current.next;
             }
+            leftmost = leftmost.left;
         }
         return root;
     }
