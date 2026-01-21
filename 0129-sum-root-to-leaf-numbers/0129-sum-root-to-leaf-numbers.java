0b87efac9c8726e25c41694e167a22bff1b82e02
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2026-01-22 02:31:58 +0530

    Time: 0 ms (100%), Space: 42.9 MB (57.86%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -15,21 +15,28 @@
  */
 class Solution {
     public int sumNumbers(TreeNode root) {
-        if (root == null)
-            return 0;
+        // if (root == null)
+        //     return 0;
         return helper(root, 0);
     }
 
     public int helper(TreeNode node, int preSum) {
-        preSum = preSum * 10 + node.val;
-        if (node.left == null && node.right == null)
-            return preSum;
-        int l = 0, r = 0;
-        if (node.left != null)
-            l = helper(node.left, preSum);
-        if (node.right != null)
-            r = helper(node.right, preSum);
+        // preSum = preSum * 10 + node.val;
+        // if (node.left == null && node.right == null)
+        //     return preSum;
+        // int l = 0, r = 0;
+        // if (node.left != null)
+        //     l = helper(node.left, preSum);
+        // if (node.right != null)
+        //     r = helper(node.right, preSum);
 
-        return l + r;
+        // return l + r;
+        
+        if(node == null)return 0;
+
+        preSum = preSum * 10 + node.val;
+        if(node.left == null && node.right == null)return preSum;
+        
+        return helper(node.left,preSum) + helper(node.right,preSum);
     }
 }
