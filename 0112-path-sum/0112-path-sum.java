cab1bdbd4b7674188a7011b371f4d36c964546c2
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2026-01-22 02:14:51 +0530

    Time: 0 ms (100%), Space: 45.1 MB (21.66%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -14,14 +14,20 @@
  * }
  */
 class Solution {
+    // public boolean hasPathSum(TreeNode root, int targetSum) {
+    //     if(root == null)return false;
+    //     return helper(root,targetSum);
+    // }
+    // public boolean helper(TreeNode root, int targetSum) {
+    //     if(root == null)return false;
+    //     int v = root.val;
+    //     if(root.left == null && root.right == null && targetSum - v == 0)return true;
+    //     return helper(root.left,targetSum - root.val) || helper(root.right,targetSum - root.val);
+    // }
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root == null)return false;
-        return helper(root,targetSum);
-    }
-    public boolean helper(TreeNode root, int targetSum) {
         if(root == null)return false;
         int v = root.val;
         if(root.left == null && root.right == null && targetSum - v == 0)return true;
-        return helper(root.left,targetSum - root.val) || helper(root.right,targetSum - root.val);
+        return hasPathSum(root.left,targetSum - root.val) || hasPathSum(root.right,targetSum - root.val);
     }
 }
