864389c5d152d0c69fc2402b09d7394faf65c78c
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2026-02-04 02:02:36 +0530

    Time: 0 ms (100%), Space: 44.8 MB (50.63%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.java
+++ 3637-trionic-array-i/3637-trionic-array-i.java
@@ -1,24 +1,45 @@
 class Solution {
     public boolean isTrionic(int[] nums) {
-        boolean firstInc = false;
-        boolean firstDec = false;
-        boolean secondInc = false;
+        // boolean firstInc = false;
+        // boolean firstDec = false;
+        // boolean secondInc = false;
 
-        for(int i = 1;i<nums.length;i++){
-            if(nums[i] > nums[i-1]){
-                if(!firstDec){
-                    firstInc = true;
-                }else{
-                    secondInc = true;
-                }
-            }else if(nums[i] < nums[i-1]){
-                if(!firstInc || secondInc){
-                    return false;
-                }else{
-                    firstDec = true;
-                }
-            }else return false;
+        // for(int i = 1;i<nums.length;i++){
+        //     if(nums[i] > nums[i-1]){
+        //         if(!firstDec){
+        //             firstInc = true;
+        //         }else{
+        //             secondInc = true;
+        //         }
+        //     }else if(nums[i] < nums[i-1]){
+        //         if(!firstInc || secondInc){
+        //             return false;
+        //         }else{
+        //             firstDec = true;
+        //         }
+        //     }else return false;
+        // }
+        // return firstInc && secondInc && firstDec;
+
+        int p = 0;
+        int n = nums.length;
+
+        while(p < n-1 && nums[p] < nums[p+1]){// Strictly Increasing.
+        p++;
         }
-        return firstInc && secondInc && firstDec;
+        if(p==0)return false;// if strictly increasing found.
+
+        int q = p;
+        while(q < n-1 && nums[q] > nums[q+1]){
+            q++;
+        }
+        if(p == q) return false;
+
+        p = q;
+        while(p < n-1 && nums[p] < nums[p+1]){
+            p++;
+        }
+        if(p==q || p != n-1)return false;
+        return true;
     }
 }
