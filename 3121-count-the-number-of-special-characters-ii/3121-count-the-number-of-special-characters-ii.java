84536c3965dd12179cc03592049a203ad1dd12c2
Bishal Singh <121380342+bishal292@users.noreply.github.com>
2026-05-27 09:39:57 +0530

    Time: 22 ms (50%), Space: 47.9 MB (85.54%) - LeetHub

---
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int ans = 0;
+
+        int[] lc = new int[26];
+        int[] uc = new int[26];
+        for(int i = 0;i<26;i++){
+            lc[i] = -1;
+            uc[i] = -1;
+        }
+        for(int i = 0;i<word.length();i++){
+            char c = word.charAt(i);
+            if(Character.isLowerCase(c)){
+                lc[c-97] = i;
+            }else if(uc[c-65] == -1){
+                uc[c-65] = i;
+            }
+        }
+        for(int i = 0;i<26;i++){
+            if(lc[i] != -1 && uc[i] != -1 && lc[i] < uc[i])ans++;
+        }
+        return ans;
+    }
+}
